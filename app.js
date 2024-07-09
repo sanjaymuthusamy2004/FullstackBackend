3bb51aae7640acab545240b85e30be2c752a4766
sanjaymuthusamy2004 <sanjay.m192004@gmail.com>
2024-07-09 20:33:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import mongoose from "mongoose";
+
+import userRouter from "./routes/user-router.js";
+import adminRouter from "./routes/admin-router.js";
+import movieRouter from "./routes/movie-router.js";
+import bookingsRouter from "./routes/booking-router.js";
+import cors from "cors";
+
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Welcome to the Movie Booking App API');
+});
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use("/user", userRouter);
+app.use("/admin", adminRouter);
+app.use("/movie", movieRouter);
+app.use("/booking", bookingsRouter);
+
+mongoose
+  .connect(
+    
+
+    `mongodb+srv://sanjay:${process.env.MONGODB_PASSWORD}@cluster0.rpjmbgv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() =>
+    app.listen(5000, () =>
+      console.log("Connected To Database And Server is running")
+    )
+  )
+  .catch((e) => console.log(e));
---
+++ routes/booking-router.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { deleteBooking, getBookingById, newBooking } from "../controller/booking-controller.js";
+
+const bookingsRouter = express.Router();
+
+bookingsRouter.get("/:id", getBookingById);
+bookingsRouter.post("/", newBooking);
+bookingsRouter.delete("/:id", deleteBooking);
+
+export default bookingsRouter;
